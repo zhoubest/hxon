e754394f4c64a3ac7aa791c0768d86dfe0ed4a65
zhoubest <zhouxiaoyangbest@163.com>
2022-04-13 14:21:13 +0800

    update http api

--- custom_components/hxon/http.py
+++ custom_components/hxon/http.py
@@ -11,7 +11,7 @@ from .const import (
     ADD_TOPIC_URL,
     DEL_TOPIC_URL,
     FETCH_TOPICS_URL,
-    RENAME_TOPIC_URL,
+    EDIT_TOPIC_URL,
     TOPIC_PREFIX,
     VALIDATE_USER_URL
 )
@@ -41,10 +41,10 @@ class HxonHttp:
                 VALIDATE_USER_URL,
                 data=json.dumps(data),headers=headers
         ) as res:
-            _LOGGING.error(res)
+            # _LOGGING.error(res)
             res.raise_for_status()
             res_dict = await res.json(content_type="application/json", encoding="utf-8")
-            _LOGGING.error(res_dict)
+            # _LOGGING.error(res_dict)
             if res_dict["code"] == 200 and res_dict["msg"] == "操作成功":
                 self._token = res_dict["token"]
                 return True
@@ -54,15 +54,16 @@ class HxonHttp:
     async def async_fetch_all_topics(self) -> dict[str, str]:
         """Fetch all topics created by us from hxon service."""
         headers = {'content-type': 'application/json',"Authorization": "Bearer "+self._token }
-        _LOGGING.error(headers)
+        # _LOGGING.error(headers)
         session = async_get_clientsession(self._hass)
         async with session.get(
                 FETCH_TOPICS_URL,
                 headers=headers
         ) as res:
-            _LOGGING.error(res)
+            # _LOGGING.error(res)
             res.raise_for_status()
             res_dict = await res.json(content_type="application/json", encoding="utf-8")
+            _LOGGING.error("followed info is res_dict of fetch all topic")
             _LOGGING.error(res_dict)
             if res_dict["code"] == 200:
                 return {
@@ -76,43 +77,56 @@ class HxonHttp:
         """Add a topic to hxon service."""
         if not topic.startswith(TOPIC_PREFIX):
             return
+        headers = {'content-type': 'application/json', "Authorization": "Bearer " + self._token}
+        data = {
+            "devName": name,
+            "devTopic": topic,
+            "devUser": self._username
+        }
+        _LOGGING.error(headers)
         session = async_get_clientsession(self._hass)
-        await session.post(
+        async with session.post(
             ADD_TOPIC_URL,
-            data={
-                "devId": 2,
-                "devName": "客厅侧灯",
-                "devTopic": "hxon/hxon0001/121wef0002",
-                "devType": "0002",
-                "devUser": "hxon0001",
-            },
-        )
+            headers=headers,
+            data=json.dumps(data),
+        ) as res:
+            res.raise_for_status()
+            res_dict = await res.json(content_type="application/json", encoding="utf-8")
+            _LOGGING.error("followed info is res_dict of async_add_topic")
+            _LOGGING.error(res_dict)
 
-    async def async_rename_topic(self, topic: str, name: str) -> None:
-        """Rename a topic in hxon service."""
+    async def async_update_topic(self, topic: str, name: str) -> None:
+        """Update a topic in hxon service."""
         if not topic.startswith(TOPIC_PREFIX):
             return
+        headers = {'content-type': 'application/json', "Authorization": "Bearer " + self._token}
+        data = {
+            "devName": name,
+            "devTopic": topic,
+            "devUser": self._username
+        }
         session = async_get_clientsession(self._hass)
-        await session.post(
-            RENAME_TOPIC_URL,
-            data={
-                "uid": self._token,
-                "topic": topic,
-                "type": 1,
-                "name": name,
-            },
-        )
+        async with session.post(
+            EDIT_TOPIC_URL,
+            headers=headers,
+            data=json.dumps(data)
+        ) as res:
+            res.raise_for_status()
+            res_dict = await res.json(content_type="application/json", encoding="utf-8")
+            _LOGGING.error("followed info is res_dict of async_rename_topic")
+            _LOGGING.error(res_dict)
 
     async def async_del_topic(self, topic: str) -> None:
         """Delete a topic from hxon service."""
         if not topic.startswith(TOPIC_PREFIX):
             return
+        headers = {'content-type': 'application/json', "Authorization": "Bearer " + self._token}
         session = async_get_clientsession(self._hass)
-        await session.post(
-            DEL_TOPIC_URL,
-            data={
-                "uid": self._token,
-                "topic": topic,
-                "type": 1,
-            },
-        )
+        async with session.get(
+            DEL_TOPIC_URL+"?topic="+topic,
+            headers=headers
+        ) as res:
+            res.raise_for_status()
+            res_dict = await res.json(content_type="application/json", encoding="utf-8")
+            _LOGGING.error("followed info is res_dict of async_del_topic")
+            _LOGGING.error(res_dict)
